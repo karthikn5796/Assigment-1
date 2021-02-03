3b02291be83da0c3562ab53b953d571775e0e096
Karthi <Karthi@DESKTOP-CP0DPRA>
2021-02-03 22:31:18 +0530

    Car.pom

--- CarWale/src/main/java/carWale/carWale.java
+++ CarWale/src/main/java/carWale/carWale.java
@@ -4,12 +4,7 @@ import java.time.Duration;
 import java.util.List;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
